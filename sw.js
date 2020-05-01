0b42db0b7918dd27cf61e0c0f74bab00d80ff13a
ukonn_admin <fujiman8@gmail.com>
2020-05-01 13:22:55 +0900

    TC

--- sw.js
+++ sw.js
@@ -220,7 +220,7 @@ var __wpo = {
  },
  strategy: 'changed',
  responseStrategy: 'cache-first',
- version: '2020-3-8 11:35:06',
+ version: '2020-4-8 11:35:06',
  name: 'webpack-offline',
  pluginVersion: '5.0.7',
  relativePaths: true
